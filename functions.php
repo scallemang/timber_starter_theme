fbd0b0bd0497f1c39cb778828f72842ee7cbd865
laras126 <laras126@gmail.com>
2015-10-26 18:10:32 -0400

    fix scripts file 404

--- functions.php
+++ functions.php
@@ -69,7 +69,7 @@ function my_scripts() {
 
 	// Enqueue stylesheet and JS file with a jQuery dependency.
 	wp_enqueue_style( 'my-styles', get_template_directory_uri() . '/assets/css/main.css', 1.0);
-	wp_enqueue_script( 'my-js', get_template_directory_uri() . '/assets/js/scripts.js', array('jquery'), '1.0.0', true );
+	wp_enqueue_script( 'my-js', get_template_directory_uri() . '/assets/js/main.js', array('jquery'), '1.0.0', true );
 }
 
 add_action( 'wp_enqueue_scripts', 'my_scripts' );
